528b826cd4df39285755550f2658bee993c72419
wintersun <alex_panciu94@yahoo.com>
2022-11-16 21:02:32 +0100

    <RPS finished>

--- main.js
+++ main.js
@@ -13,57 +13,56 @@ function playRound(playerSelection, computerSelection) {
         return "Invalid input! You have to pick Rock, Paper or Scissors!";
     }
 
-    console.log("You picked: " + playerSelection + "\n", "The computer picked: " + computerSelection);
-    
+    inputPlayer.textContent = "You picked: " + playerSelection + "\n"
+    inputComputer.textContent = "The computer picked: " + computerSelection;
     if (playerSelection == "rock" && computerSelection == "scissors") {
 
         playerWins = true;
         computerWins = false;
-        return "You win! Rock beats Scissors";
+        results.textContent = "You win! Rock beats Scissors";
 
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
 
         playerWins = false;
         computerWins = true;
-        return "You lose! Rock beats Scissors";
+        results.textContent = "You lose! Rock beats Scissors";
 
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
 
         playerWins = true;
         computerWins = false;
-        return "You win! Scissors beats Paper";
+        results.textContent = "You win! Scissors beats Paper";
 
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
 
         playerWins = false;
         computerWins = true;
-        return "You lose! Scissors beats Paper";
+        results.textContent = "You lose! Scissors beats Paper";
 
     } else if (playerSelection == "paper" && computerSelection == "rock") {
 
         playerWins = true;
         computerWins = false;
-        return "You win! Paper beats Rock";
+        results.textContent = "You win! Paper beats Rock";
 
     } else if (playerSelection == "rock" && computerSelection == "paper") {
 
         playerWins = false;
         computerWins = true;
-        return "You lose! Paper beats Rock";
+        results.textContent = "You lose! Paper beats Rock";
 
     } else { // i.e. playerSelection === computerSelection
 
         playerWins = false;
         computerWins = false;
-        return "Draw! You both picked " + playerSelection[0].toUpperCase() + playerSelection.slice(1); //only doing this for styling (want it to be consistently capitalised)
+        results.textContent = "Draw! You both picked " + playerSelection[0].toUpperCase() + playerSelection.slice(1); //only doing this for styling (want it to be consistently capitalised)
 
     }
 }
 
+/*
 let handsWon = 0, computerHandsWon = 0;
-function game () {
     for (round = 1; round <= 5; round++) { //iterates through 5 rounds
-        console.log(playRound(prompt("Rock, paper or scissors?").toLowerCase(), computerPlay())); 
         if (playerWins) {
             handsWon++;
         } else if (computerWins) {
@@ -71,10 +70,41 @@ function game () {
         }
         //if they're both false it's a draw, neither gets a point 
     }
-    console.log(`You won ${handsWon} rounds, whereas the computer won ${computerHandsWon}. There were ${5 - handsWon - computerHandsWon} draws.`)
+    final.textContent = `You won ${handsWon} rounds, whereas the computer won ${computerHandsWon}. There were ${5 - handsWon - computerHandsWon} draws.`;
     if (handsWon > computerHandsWon) {
-        console.log("You win overall!");
+        win.textContent = "You win overall!";
     } else {
-        console.log("You lose overall! Better luck next time!");
+        lose.textContent = "You lose overall! Better luck next time!";
     }
-}
+*/
+
+let handsWon = 0, computerHandsWon = 0, rounds = 0;
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (rounds < 5){
+            playRound(button.id, computerPlay());
+            if (playerWins) {
+                handsWon++;
+                playerwon.textContent = `You won ${handsWon} rounds`; 
+            } else if (computerWins) {
+                computerHandsWon++;
+                computerwon.textContent = `The computer won ${computerHandsWon} rounds`;
+            } 
+            rounds++;
+        };
+        if (handsWon > computerHandsWon && rounds == 5) {
+            player_result.textContent = "You win overall!";
+            
+        } else if (handsWon < computerHandsWon && rounds == 5){
+            computer_result.textContent = "You lose overall! Better luck next time!";
+        } else if (handsWon == computerHandsWon && rounds == 5) {
+            tie.textContent = "It's a tie";
+    };
+  });
+ });
+
+
+
+
